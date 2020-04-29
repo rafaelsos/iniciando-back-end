26e7f9b15c077f2738e82ccfcf6442b4931216bc
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-29 20:36:42 -0300

    relationship in models

--- src/database/migrations/1588131613008-CreateAppointments.ts
+++ src/database/migrations/1588131613008-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1588131613008
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',

39488f337bd401b9da5988ac3e0431dc3a047014
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-30 00:02:12 -0300

    Validating credentials

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.route';
 import usersRouter from './users.route';
+import sessionsRouter from './sessions.route';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;

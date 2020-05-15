66fa9e4ed17f774cec88befa5a9a8fbf8c3b3265
Rafael Sosnowski Totvs <rafael.sosnowski@totvs.com.br>
2020-05-15 18:07:18 -0300

    architecture and DDD,

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import AppError from '../errors/AppError';
-import Appointment from '../models/Appointment';
-import AppointmentRepository from '../reposytories/AppointmentsRepository';
+import AppError from '@shared/errors/AppError';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentRepository from '@modules/appointments/reposytories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
